3588fdd0dd8f2fdf3942be0274b5b2bf5797f137
Dmitriy Filippov <Amoralis.19@gmail.com>
2021-07-15 20:42:05 +0300

    1

--- js/calc.js
+++ js/calc.js
@@ -19,15 +19,37 @@ jQuery(document).ready(function ($) {
     }
   });
 
-
+  
   data.forEach((elem, i) => {
-    let newOption = document.createElement('option');
-    newOption.value = i;
-    newOption.dataset.latitude = elem['geo_lat'];
-    newOption.dataset.longitude = elem['geo_lon'];
-    newOption.dataset.sunset = elem['timezone'];
-    newOption.textContent = elem['address'];
-    select.appendChild(newOption);
+    if (i == 0) {
+      let newOption2 = document.createElement('option');
+      newOption2.value = 'none val';
+      newOption2.classList.add('pointer-events');
+      newOption2.dataset.latitude = 0.00;
+      newOption2.dataset.longitude = 0.00;
+      newOption2.dataset.sunset = elem['timezone'];
+      newOption2.textContent = 'Выберите город';
+      select.appendChild(newOption2);
+
+      let newOption = document.createElement('option');
+      newOption.value = i;
+      newOption.dataset.latitude = elem['geo_lat'];
+      newOption.dataset.longitude = elem['geo_lon'];
+      newOption.dataset.sunset = elem['timezone'];
+      newOption.textContent = elem['address'];
+      select.appendChild(newOption);
+
+
+    } else {
+      let newOption = document.createElement('option');
+      newOption.value = i;
+      newOption.dataset.latitude = elem['geo_lat'];
+      newOption.dataset.longitude = elem['geo_lon'];
+      newOption.dataset.sunset = elem['timezone'];
+      newOption.textContent = elem['address'];
+      select.appendChild(newOption);
+    }
+    
   });
 
   
@@ -43,9 +65,14 @@ jQuery(document).ready(function ($) {
   var currentdate = new Date();
   var chart_data = {
     "zero": [[currentdate.getFullYear()+"-01-01",0], [currentdate.getFullYear()+"-12-31",0]], 
+    "sunset": [[currentdate.getFullYear()+"-01-01", 0], [currentdate.getFullYear()+"-6-31", 0], [currentdate.getFullYear()+"-12-31", 0]], 
+    "sunrise": [[currentdate.getFullYear()+"-01-01", 0], [currentdate.getFullYear()+"-6-31", 0], [currentdate.getFullYear()+"-12-31", 0]]
+  };
+  /* var chart_data = {
+    "zero": [[currentdate.getFullYear()+"-01-01",0], [currentdate.getFullYear()+"-12-31",0]], 
     "sunset": [[currentdate.getFullYear()+"-01-01", 500], [currentdate.getFullYear()+"-6-31", 200], [currentdate.getFullYear()+"-12-31", 500]], 
     "sunrise": [[currentdate.getFullYear()+"-01-01", -500], [currentdate.getFullYear()+"-6-31", -200], [currentdate.getFullYear()+"-12-31", -500]]
-  };
+  }; */
   /*ticks: [
     ['-1200', '12:00'], 
     ['-1100', '11:00'], 
@@ -74,14 +101,17 @@ jQuery(document).ready(function ($) {
     ['1200', '12:00']],*/
   var timesSunrise;
   var timesSunset;
+  var sunriseTable;
+  var sunsetTable;
+  var times;
 
   $('.js-select2').on('select2:select', () => {
 
-    var times = SunCalc.getTimes(new Date(), 
+    times = SunCalc.getTimes(new Date(), 
     select.options[select.selectedIndex].dataset.latitude, 
     select.options[select.selectedIndex].dataset.longitude);
 
-    var sunriseTable = -120;
+    sunriseTable = -120;
 
     if (times.sunrise.getHours() == 0) {
       sunriseTable = 0;
@@ -107,7 +137,7 @@ jQuery(document).ready(function ($) {
       sunriseTable = -600;
     }
     
-    var sunsetTable = 120;
+    sunsetTable = 120;
 
     if (times.sunset.getHours() == 0) {
       sunsetTable = 0;
@@ -172,14 +202,17 @@ jQuery(document).ready(function ($) {
 
     /* chart_data["sunset"] = +timesSunset;
     chart_data["sunrise"] = +timesSunrise; */
-    console.log(timesSunset)
+    /* console.log(timesSunset)
     console.log(sunsetTable)
     console.log(timesSunrise)
     console.log(sunriseTable)
-    console.log(times.sunrise.getHours())
+    console.log(times.sunrise.getHours()) */
       
-    drawChartSun();
+    
 
+    
+    drawChartSun();
+    dimmging();
   });
 
 
@@ -293,10 +326,7 @@ function drawChartSun()
 		//plot1.replot();
 	});
 }
-
 drawChartSun();
-
-var chartDimm;
 
 var dimm_data = [
   ['00', 100], 
@@ -325,19 +355,89 @@ var dimm_data = [
   ['23', 90]
 ];
 
+const dimInpFor = document.querySelectorAll('.dimming');
+
+  dimInpFor.forEach((elem, i) => {
+    dimm_data[i][1] = elem.value;
+  });
+
+function dimmging() {
+  let sunrise = times.sunrise.getHours(), 
+      sunset = times.sunset.getHours(),
+      count = 80;
+  
+
+  for (let i = 0; i < dimm_data.length; i++) {
+    
+    
+    if (i > sunrise && i < sunset) {
+      if (i < (sunset - sunrise) / 2) {
+        dimm_data[i][1] = count;
+        count -= 10;
+      } else if (i < sunset - 4) {
+        dimm_data[i][1] = 30;
+      } else {
+        dimm_data[i][1] = count;
+        count += 10;
+      }
+    } else {
+      dimm_data[i][1] = 100;
+    }
+  }
+
+  dimInpFor.forEach((elem, i) => {
+    elem.value = dimm_data[i][1];
+  });
+
+  /* dimm_data[sunrise][1] = 100;
+  dimm_data[sunset][1] = 100; */
+  
+  
+  /* dimm_data = [
+    ['00', 100], 
+    ['01', 100], 
+    ['02', 100], 
+    ['03', 90], 
+    ['04', 80], 
+    ['05', 80], 
+    ['06', 60], 
+    ['07', 50], 
+    ['08', 50], 
+    ['09', 90], 
+    ['10', 90],
+    ['11', 90],
+    ['12', 90],
+    ['13', 90],
+    ['14', 90],
+    ['15', 90],
+    ['16', 90],
+    ['17', 90],
+    ['18', 90],
+    ['19', 90],
+    ['20', 90],
+    ['21', 90],
+    ['22', 90],
+    ['23', 90]
+  ]; */
+
+  drawDimmChart();
+}
+
+
+var chartDimm;
+
+
+
 var ticks2 = [
   ["0", 0], 
   ["20", 20], 
   ["40", 40],
   ["60", 60], 
   ["80", 80], 
-  ["100", 100]];
+  ["100", 100]
+];
 
-  const dimInpFor = document.querySelectorAll('.dimming');
-
-  dimInpFor.forEach((elem, i) => {
-    dimm_data[i][1] = elem.value;
-  })
+  
 
 var dimInputs = $('.calc-dim-table-wrap input');
 
@@ -368,7 +468,7 @@ function drawDimmChart()
 			chartDimm.destroy();
 
 		chartDimm = $.jqplot('chart2', [dimm_data], {
-			title:'Расчет диммирования',
+			title:'Диммирование',
 			/* animate : true, */
 			seriesDefaults: {
 				rendererOptions: {
@@ -406,63 +506,80 @@ drawDimmChart();
 
 
 
-  $('.add').on('click', function() {
-    let newRow = document.createElement('div');
-    const rowWrap = document.querySelector('.section-one');
+$('.add').on('click', function() {
+  let newRow = document.createElement('div');
+  const rowWrap = document.querySelector('.section-one');
 
-    newRow.classList.add('calc-form-row');
-    newRow.innerHTML = `
-      <div class="calc-form-row-item">
-        <label for="">Тип лампы:</label>
-        <div>
-          <select name="" id="">
-            <option value="1" selected>Светодиодная</option>
-            <option value="2">Светодиодная 0-10V</option>
-            <option value="3">Светодиодная DALI</option>
-          </select>
-        </div>
+  newRow.classList.add('calc-form-row');
+  newRow.innerHTML = `
+    <div class="calc-form-row-item">
+      <label for="">Тип лампы:</label>
+      <div>
+        <select name="" id="">
+          <option value="1" selected>Светодиодная</option>
+          <option value="2">Светодиодная 0-10V</option>
+          <option value="3">Светодиодная DALI</option>
+        </select>
       </div>
-      <div class="calc-form-row-item">
-        <label for="">Кол-во:</label>
-        <div>
-          <input type="text" value="1">
-        </div>
+    </div>
+    <div class="calc-form-row-item">
+      <label for="">Кол-во:</label>
+      <div>
+        <input type="text" value="1">
       </div>
-      <div class="calc-form-row-item">
-        <label for="">Мощность W:</label>
-        <div>
-          <input type="text" value="100">
-        </div>
+    </div>
+    <div class="calc-form-row-item">
+      <label for="">Мощность W:</label>
+      <div>
+        <input type="text" value="100">
       </div>
-      <div class="calc-form-row-item btn-item">
-        <div class="button alt-btn remove">
-          Удалить
-        </div>
-      </div>`
+    </div>
+    <div class="calc-form-row-item btn-item">
+      <div class="button alt-btn remove">
+        Удалить
+      </div>
+    </div>`
 
-    rowWrap.appendChild(newRow);
+  rowWrap.appendChild(newRow);
 
-    const rowDelete = document.querySelectorAll('.section-one .calc-form-row');
+  const rowDelete = document.querySelectorAll('.section-one .calc-form-row');
 
-    rowDelete.forEach((elem) => {
-      const btn = elem.querySelector('.button');
-      btn.addEventListener('click', () => {
-        if (!btn.parentElement.parentElement.classList.contains('no-del')) {
-          btn.parentElement.parentElement.remove();
-        }
-      });
-    })
+  rowDelete.forEach((elem) => {
+    const btn = elem.querySelector('.button');
+    btn.addEventListener('click', () => {
+      if (!btn.parentElement.parentElement.classList.contains('no-del')) {
+        btn.parentElement.parentElement.remove();
+      }
+    });
+  })
+
+});
+
+
+$('.calc-eco-btn .button').on('click', function () { 
+  calculating();
+
+  $(this).slideUp(200);
+  $('.calc-eco-slide').slideDown(200);
+  $('.calc-eco-text').slideDown(200);
+});
+
+
+function calculating() { 
+  const lampRow = document.querySelectorAll('.calc-form-row');
+  
+  lampRow.forEach((elem) => {
+    let lampType = elem.querySelector('.lamp-select'),
+        lampCount = elem.querySelector('.lamp-count'),
+        lampWT = elem.querySelector('.lamp-wt');
+
 
   });
-
-
   
 
+}
 
-
-
-
-
+	
 
 
 
